cbe67e73c710eb6e4822a948b82eee26f059e41d
Vo Nhu Phi <nhuphivo@gmail.com>
2025-03-20 09:53:36 +0700

    change firebase config

--- src/app/third-party/config/firebaseConfig.ts
+++ src/app/third-party/config/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUTaxzczbbHM28YbIRIbFkD3TGly-2uBE",
-  authDomain: "phi-login.firebaseapp.com",
-  projectId: "phi-login",
-  storageBucket: "phi-login.firebasestorage.app",
-  messagingSenderId: "695770604217",
-  appId: "1:695770604217:web:b6d73367cc8d450455f168",
-  measurementId: "G-PT699ZYCXM",
+  apiKey: "AIzaSyDErsK-0VDAzdIKVavGAmZQGzkuVqtEa6s",
+  authDomain: "authentication-demo-34282.firebaseapp.com",
+  projectId: "authentication-demo-34282",
+  storageBucket: "authentication-demo-34282.firebasestorage.app",
+  messagingSenderId: "930036533271",
+  appId: "1:930036533271:web:1c54edc53b19ac6a5af452",
+  measurementId: "G-WNGMX4YWHW",
 };
 
 const app = initializeApp(firebaseConfig);
